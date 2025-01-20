263a0a25a2ca293680e8d1dddcb068ed98b98cd7
unknown <fmuchembi@gmail.com>
2025-01-20 22:54:15 +0100

    create file and variable to hold my dictionary file data

---
+++ data/data.py
@@ -0,0 +1,68 @@
+cities = [
+    {
+        "name": "Nairobi",
+        "country": "Kenya",
+        "location": {"latitude": -1.286389, "longitude": 36.817223},
+        "photo_url": "Images\nairobi_background.avif"
+    },
+    {
+        "name": "Cape Town",
+        "country": "South Africa",
+        "location": {"latitude": -33.924869, "longitude": 18.424055},
+        "photo_url": "Images\Capetown.jpg"
+    },
+    {
+        "name": "Berlin",
+        "country": "Germany",
+        "location": {"latitude": 52.520008, "longitude": 13.404954},
+        "photo_url": "Images\Berlin.jpg"
+    },
+    {
+        "name": "Paris",
+        "country": "France",
+        "location": {"latitude": 48.856613, "longitude": 2.352222},
+        "photo_url": "Images\Paris.jpg"
+    },
+    {
+        "name": "New York",
+        "country": "USA",
+        "location": {"latitude": 40.712776, "longitude": -74.005974},
+        "photo_url": "Images\New_york.jpg"
+    },
+    {
+        "name": "Dubai",
+        "country": "United Arab Emirates",
+        "location": {"latitude": 25.276987, "longitude": 55.296249},
+        "photo_url": "Images\Dubai.jpg"
+    },
+    {
+        "name": "Shanghai",
+        "country": "China",
+        "location": {"latitude": 31.230391, "longitude": 121.473701},
+        "photo_url": "Images\Shanghai.jpg"
+    },
+    {
+        "name": "Oslo",
+        "country": "Norway",
+        "location": {"latitude": 59.913868, "longitude": 10.752245},
+        "photo_url": "Images\Oslo.jpg"
+    },
+    {
+        "name": "New Delhi",
+        "country": "India",
+        "location": {"latitude": 28.613939, "longitude": 77.209021},
+        "photo_url": "Images\New-Delhi.jpg"
+    },
+    {
+        "name": "Bogotá",
+        "country": "Colombia",
+        "location": {"latitude": 4.7110, "longitude": -74.0721},
+        "photo_url": "Images\Bogota.jpg"
+    },
+    {
+        "name": "San Francisco",
+        "country": "USA (California)",
+        "location": {"latitude": 37.774929, "longitude": -122.419418},
+        "photo_url": "Images\San_Francisco.jpg"
+    }
+]
